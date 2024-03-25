968447b995c869a359f62b8350eb9b46d09f7776
Mateusz Głuchowski <matigluchowski0410@gmail.com>
2024-03-25 23:39:09 +0100

    Add files via upload

---
+++ RandomNumberGenerator.py
@@ -0,0 +1,58 @@
+import time
+from colorama import Style, Fore
+
+class RandomNumberGenerator:
+    __modulus: int | None = None
+    __mnoznik: int | None = None
+    __przyrost: int | None = None
+    __seed: int | None = None
+    __amount: int | None = None
+
+    def __init__(self, modulus, amount):
+        self.__modulus = modulus
+        self.__mnoznik = 250
+        self.__przyrost = 37
+        self.__amount = amount
+        self.__seed = self.generate_seed()
+
+        self.example()
+
+        if self.check_values() == False:
+            print(Fore.RED, "[-] Provided values are incorrect. Exiting...", Style.RESET_ALL)
+
+
+    def generate_sequence(self):
+        for i in range(self.__amount):
+            r_tmp = 0
+            print()
+
+    def example(self):
+        r_previous = self.__seed
+        for i in range(self.__amount):
+            val_tmp = (self.__mnoznik*r_previous + self.__przyrost) % self.__modulus
+            u_val = (val_tmp/(self.__modulus-1))*self.__modulus
+            print(Fore.GREEN, f"R{i+1} = {self.__mnoznik * r_previous + 1} mod {self.__modulus} = {int(u_val)}", Style.RESET_ALL)
+            # print(int(u_val))
+            r_previous = u_val
+
+    def generate_seed(self) -> int:
+        return int(time.time())
+    
+    def check_values(self) -> bool:
+        if self.__mnoznik <= 0:
+            False
+        
+        if self.__przyrost < 0:
+            False
+        
+        if self.__modulus <= self.__przyrost:
+            False
+
+        if self.__modulus <= self.__seed:
+            False
+
+        True
+
+
+if __name__ == '__main__':
+    rng = RandomNumberGenerator(8, 30)
